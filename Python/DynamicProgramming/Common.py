e03a736b8a52a65a47102796d2fe1e5277709abd
Zigmondy <ratheenitin@hotmail.com>
2016-05-03 14:21:17 -0700

    common code

--- Python/DynamicProgramming/Common.py
+++ Python/DynamicProgramming/Common.py
@@ -1,4 +1,16 @@
-def printMatrix(m, label = "matrix (rows={0}, columns={1}):"):
-    print(label.format(len(m), len(m[0])))
-    for row in m:
-       print(row)
+def printMatrix(m, label="Matrix - {0} x {1} - {2} x {3}:", r="", c="", symbol=""):
+    print("-----------------------------------")
+    print(label.format(len(m), len(m[0]), r, c))
+    print("-----------------------------------")
+    for rowIndex in range(len(m)):
+        buffer = ""
+        for columnIndex in range(len(m[0])):
+	        buffer = buffer + "{0}{1} ".format(symbol, m[rowIndex][columnIndex])
+        buffer = "[" + buffer.rstrip() + "]"
+        print(buffer)
+
+def printVector(v, label=""):
+    print("-----------------------------------")    
+    print("{0} - {1}".format(label, len(v)))
+    print("-----------------------------------")    
+    print(v)
--- Python/DynamicProgramming/LongestCommonSubsequence.py
+++ Python/DynamicProgramming/LongestCommonSubsequence.py
@@ -2,40 +2,46 @@ import Common
 
 word1 = ['a', 'b', 'c', 'd', 'a', 'f']
 word2 = ['a', 'c', 'b', 'c', 'f']
+seq = []
 
 columns = len(word1) + 1
 rows = len(word2) + 1
 m = [[0 for _ in range(columns)] for _ in range(rows)]
 
-for row in range(1, rows):
-    for column in range(1, columns):
-        char1 = word1[column - 1]
-        char2 = word2[row - 1]
-        if char1 == char2:
-            # Diagonal + 1
-            diagonal = m[row - 1][column - 1]
-            m[row][column] = diagonal + 1
-        else:
-            # Max of previous column or row
-            m[row][column] = max(m[row][column - 1], m[row - 1][column])
+def commonSubsequence():
+    for row in range(1, rows):
+        for column in range(1, columns):
+            char1 = word1[column - 1]
+            char2 = word2[row - 1]
+            if char1 == char2:
+                # Diagonal + 1
+                diagonal = m[row - 1][column - 1]
+                m[row][column] = diagonal + 1
+            else:
+                # Max of previous column or row
+                m[row][column] = max(m[row][column - 1], m[row - 1][column])
 
-Common.printMatrix(m)
-seq = []
-column = columns - 1
-row = rows - 1
-while column > 0 and row > 0:
-   current = m[row][column]
-   prevRow = m[row - 1][column]
-   prevColumn = m[row][column - 1]
-   prevDiagonal = m[row - 1][column - 1]
-   if current == prevColumn:
-      column = column - 1
-   elif current == prevRow:
-      row = row - 1
-   elif current == prevDiagonal + 1:
-      seq.append(word1[column - 1])
-      column = column - 1
-      row = row - 1
+def backtrack():
+    column = columns - 1
+    row = rows - 1
+    while column > 0 and row > 0:
+        current = m[row][column]
+        prevRow = m[row - 1][column]
+        prevColumn = m[row][column - 1]
+        prevDiagonal = m[row - 1][column - 1]
+        if current == prevColumn:
+            column = column - 1
+        elif current == prevRow:
+            row = row - 1
+        elif current == prevDiagonal + 1:
+            seq.append(word1[column - 1])
+            column = column - 1
+            row = row - 1
 
-print("common subsequence:")
+Common.printVector(word1, label="word1")
+Common.printVector(word2, label="word2")
+commonSubsequence()
+Common.printMatrix(m, r="word2", c="word1")
+backtrack()
+print("Common subsequence:")
 print(seq)
